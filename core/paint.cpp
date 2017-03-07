2f1b4cdedbfdb9be353b0e32dc89c54fc0215485
Alexander Bragin <agarb7@gmail.com>
2017-03-07 21:44:30 +0800

    remove waste commented code

--- core/paint.cpp
+++ core/paint.cpp
@@ -43,59 +43,6 @@ void drawSegment(const Segment &segment, Canvas &canvas)
     canvas.setColor(x2, y2, segment.color);
 }
 
-//void drawSegment2(const Segment &segment, Canvas &canvas)
-//{
-//    using std::lround;
-//    using std::max;
-//    using std::abs;
-
-//    double x1 = segment.point1.x;
-//    double y1 = segment.point1.y;
-
-//    double x2 = segment.point2.x;
-//    double y2 = segment.point2.y;
-
-//    double lx = x2-x1;
-//    double ly = y2-y1;
-
-//    auto dda = [&]()
-
-//    if (lx<ly) {
-
-//    }
-
-
-
-//    double m = (segment.point2.y-y)/(segment.point2.x-x);
-
-//    if (m<)
-
-//    int x1 = lround(x);
-//    int y1 = lround(y);
-
-//    int x2 = lround(segment.point2.x);
-//    int y2 = lround(segment.point2.y);
-
-//    int lx = x2-x1;
-//    int ly = y2-y1;
-
-//    int c = max(abs(lx), abs(ly))+1;
-
-//    double dx = double(lx)/c;
-//    double dy = double(ly)/c;
-
-//    canvas.setColor(x1, y1, segment.color);
-
-//    for (int i=1, last = c-2; i<=last; ++i) {
-//        x+=dx;
-//        y+=dy;
-
-//        canvas.setColor(lround(x), lround(y), segment.color);
-//    }
-
-//    canvas.setColor(x2, y2, segment.color);
-//}
-
 void paint(const Scene &scene, Canvas &canvas)
 {
     for (auto it = scene.segmentBegin(), end = scene.segmentEnd();
